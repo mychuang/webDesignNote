d83c02a96bd8b3beb89aae964dc5b1a744a9645c
Miller.Huang <Miller.Huang@lumens.com.tw>
2021-11-01 10:33:22 +0800

    Delete the unused folder

--- webDesigeSample/main.js
+++ webDesigeSample/main.js
@@ -1,24 +1,8 @@
 $(document).ready(function(){
 
     $("#addButton").click(function(){
-        $("#addModal").modal("show");
-        $("#addModalHeader").on("mousedown", function(mousedownEvt) {
-            var $draggable = $(this);
-            var x = mousedownEvt.pageX - $draggable.offset().left,
-                y = mousedownEvt.pageY - $draggable.offset().top;
-            $("body").on("mousemove.draggable", function(mousemoveEvt) {
-                $draggable.closest(".modal-dialog").offset({
-                    "left": mousemoveEvt.pageX - x,
-                    "top": mousemoveEvt.pageY - y
-                });
-            });
-            $("body").one("mouseup", function() {
-                $("body").off("mousemove.draggable");
-            });
-            $draggable.closest(".modal").one("bs.modal.hide", function() {
-                $("body").off("mousemove.draggable");
-            });
-        });
+        $('#addModal').modal('show')
+              .draggable();
     });
 
 });
