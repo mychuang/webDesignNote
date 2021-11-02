4f28ffffb67052ff477f9070b64cf65e2e0d1f48
Miller.Huang <Miller.Huang@lumens.com.tw>
2021-11-02 10:46:33 +0800

    Delete the testing folder

--- jquery-begin/main.js
+++
@@ -1,8 +0,0 @@
-//window.onload = function(){alert("Hello jQuery")};
-$(document).ready(function(){
-    $("a").addClass("test");
-    $("a").click(function(event){
-        alert("Hello jQuery");
-        event.preventDefault();
-    });
-});
